2725ae394850b2333d9fc4604816bf77145fcb5e
LanielleP <nellysbookshelf@gmail.com>
2021-01-15 19:52:34 -0800

    Some of the changes needed in to use webcam.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shirley/StolenFromHollyBots/RingPosition.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Shirley.StolenFromHollyBots;
+
+/*
+ * An enum to define the skystone position
+ */
+public enum RingPosition
+{
+    UNKNOWN,
+    FOUR,
+    ONE,
+    NONE
+}
