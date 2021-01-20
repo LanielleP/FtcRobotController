7e546d7db2a58dceda30bee719755e71297d4751
Chronoportation <georgiapetroff@gmail.com>
2021-01-20 14:32:53 -0800

    HighGoal_AUTO now includes "@Autonomous"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/HighGoal_AUTO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/HighGoal_AUTO.java
@@ -7,12 +7,14 @@
 
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Autonomous
 public class HighGoal_AUTO extends LinearOpMode {
     //prepares needed hardware
     private DcMotor driveRF;//drive wheel located RIGHT FRONT
